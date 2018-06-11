5af96b3765cfe00ea4cae4f6b2f7dad0b0f9e3a1
Nebera Anton <anebera@apextech.com.ua>
2018-06-11 13:46:00 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+@import "./assets/scss/colors.scss";
+@import "./assets/scss/fonts.scss";
+@import "./assets/scss/media.scss";
+@import "./assets/scss/sprite.scss";
+@import "./assets/scss/global.scss";
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
